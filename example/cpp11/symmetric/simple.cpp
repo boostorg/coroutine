1fde4d22196aac679edf7d27b7289a93ced4e2e2
Oliver Kowalke <oliver.kowalke@gmx.de>
2014-02-03 20:09:51 +0100

    c++11 examples for symmetric coroutines

---
+++ example/cpp11/symmetric/simple.cpp
@@ -0,0 +1,41 @@
+
+//          Copyright Oliver Kowalke 2009.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <cstdlib>
+#include <iostream>
+
+#include <boost/coroutine/all.hpp>
+
+typedef boost::coroutines::symmetric_coroutine< void >  coro_t;
+
+coro_t * c1 = 0;
+coro_t * c2 = 0;
+
+int main( int argc, char * argv[])
+{
+    coro_t coro1(
+        [&]( coro_t::self_type & self) {
+            std::cout << "foo1" << std::endl;
+            self( * c2);
+            std::cout << "foo2" << std::endl;
+            self( * c2);
+            std::cout << "foo3" << std::endl;
+        });
+    coro_t coro2(
+        [&]( coro_t::self_type & self) {
+            std::cout << "bar1" << std::endl;
+            self( * c1);
+            std::cout << "bar2" << std::endl;
+            self( * c1);
+            std::cout << "bar3" << std::endl;
+        });
+    c1 = & coro1;
+    c2 = & coro2;
+    coro1();
+    std::cout << "Done" << std::endl;
+
+    return EXIT_SUCCESS;
+}
