7779116598972c5cecdf053d265df2412658e3ea
Nikita Kniazev <nok.raven@gmail.com>
2019-12-02 01:34:45 +0300

    Missed begin/end. Follow-up to PR#43
    I did not notice that begin/end indirectly uses range_begin/range_end.

--- include/boost/coroutine/asymmetric_coroutine.hpp
+++ include/boost/coroutine/asymmetric_coroutine.hpp
@@ -2361,32 +2361,32 @@ struct coroutine
 template< typename R >
 typename pull_coroutine< R >::iterator
 begin( pull_coroutine< R > & c)
-{ return boost::begin( c); }
+{ return coroutines::range_begin( c); }
 
 template< typename R >
 typename pull_coroutine< R >::const_iterator
 begin( pull_coroutine< R > const& c)
-{ return boost::begin( c); }
+{ return coroutines::range_begin( c); }
 
 template< typename R >
 typename pull_coroutine< R >::iterator
 end( pull_coroutine< R > & c)
-{ return boost::end( c); }
+{ return coroutines::range_end( c); }
 
 template< typename R >
 typename pull_coroutine< R >::const_iterator
 end( pull_coroutine< R > const& c)
-{ return boost::end( c); }
+{ return coroutines::range_end( c); }
 
 template< typename R >
 typename push_coroutine< R >::iterator
 begin( push_coroutine< R > & c)
-{ return boost::begin( c); }
+{ return coroutines::range_begin( c); }
 
 template< typename R >
 typename push_coroutine< R >::iterator
 end( push_coroutine< R > & c)
-{ return boost::end( c); }
+{ return coroutines::range_end( c); }
 
 }
 
